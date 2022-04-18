51b8cc8b128fc4bd776453565bd65d231c391582
Enrique Castañeda <des_externo09@ccontrol.com.mx>
2022-04-18 00:27:55 -0600

    se reparo el error de auth en api de configuracion

--- services/generarFechaVigencia.js
+++ services/generarFechaVigencia.js
@@ -8,24 +8,24 @@ const PASS = process.env.AUTH_PASS;
 
 const generarFechaVigencia = async (fechaPedido) => {
     fPedido = new Date(fechaPedido + "Z");
-    diasVigencia = getVigencia();
-    fPedido.setDate(fPedido.getDate() + diasVigencia);
-
+    diasVigencia = await getVigencia();
+    console.log(fPedido)
+    console.log(diasVigencia)
+    fPedido.setDate(fPedido.getDate() + parseInt(diasVigencia));
+    console.log(fPedido)
     return fPedido;
 }
 
 
 const getVigencia = async () => {
+
     const result = await axios(
         {
             method: 'post',
             url: API_URL,
             data: {
                 Clave: CLAVE
-            }
-        },
-        {},
-        {
+            },       
             auth: {
                 username: USER,
                 password: PASS
@@ -37,5 +37,6 @@ const getVigencia = async () => {
 
     return result;
 }
+
 
 module.exports = generarFechaVigencia;
